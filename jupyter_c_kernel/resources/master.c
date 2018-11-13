6140d117db7ebfe7627af58829b13b08b22cdae2
quantlab <quantlab.io@gmail.com>
2018-11-13 14:08:45 +0800

    replace dlfcn.h with windows.h
    * replace `dlopen` with `LoadLibrary`
    * replace `dlsym` with `GetProcAddress`
    * replace `dlerror` with `GetLastError`

--- jupyter_c_kernel/resources/master.c
+++ jupyter_c_kernel/resources/master.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <dlfcn.h>
+//#include <dlfcn.h>
+#include <Windows.h>
 
 typedef int (*main_t)(int, char **, char **);
 
@@ -14,14 +15,17 @@ int main(int argc, char **argv, char **envp)
         fprintf(stderr, "USAGE: %s PROGRAM\nWhere PROGRAM is the user's program to supervise\n", argv[0]);
         return EXIT_FAILURE;
     }
-    void *userhandle = dlopen(argv[1], RTLD_LAZY);
+    //void *userhandle = dlopen(argv[1], RTLD_LAZY);
+    void *userhandle = LoadLibrary(argv[1]);
     if (userhandle == NULL) {
-        fprintf(stderr, "%s: %s\n", argv[0], dlerror());
+        //fprintf(stderr, "%s: %s\n", argv[0], dlerror());
+        fprintf(stderr, "%s: %s\n", argv[0], GetLastError());
         return EXIT_FAILURE;
     }
-    dlerror();
-    main_t usermain = dlsym(userhandle, "main");
-    if ((error = dlerror()) != NULL) {
+    //dlerror();
+    //main_t usermain = dlsym(userhandle, "main");
+    main_t usermain = GetProcAddress(userhandle, "main");
+    if ((error = GetLastError()) != NULL) {
         fprintf(stderr, "%s: %s\n", argv[0], error);
         return EXIT_FAILURE;
     }
